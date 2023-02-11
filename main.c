ff07f6cffc4a53c569ba3d6b5752d5d56d0346f1
Max Ruts <tribar.ge@gmail.com>
2023-02-11 04:48:34 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <libudev.h>
+
+void hid_open(unsigned short vendor_id, unsigned short product_id)
+{
+   struct udev* udev;
+   struct udev_enumerate* enumerate;
+   struct udev_list_entry* devices;
+   struct udev_list_entry* dev_list_entry;
+   char device[128];
+
+   udev = udev_new();
+   if(!udev) {
+       printf("Error\n");
+       // TODO: exit;
+   }
+
+   enumerate = udev_enumerate_new(udev);
+   if(!enumerate) {
+       printf("Error\n");
+       // TODO: exit;
+   }
+
+
+
+}
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
