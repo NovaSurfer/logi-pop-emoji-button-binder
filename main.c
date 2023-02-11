35dbef4d3a3cc3e5fc1efd7d4dce2effaabc583f
Max Ruts <tribar.ge@gmail.com>
2023-02-11 05:05:02 +0100

    Adds code formatting

--- main.c
+++ main.c
@@ -1,31 +1,29 @@
-#include <stdio.h>
 #include <libudev.h>
+#include <stdio.h>
 
 void hid_open(unsigned short vendor_id, unsigned short product_id)
 {
-   struct udev* udev;
-   struct udev_enumerate* enumerate;
-   struct udev_list_entry* devices;
-   struct udev_list_entry* dev_list_entry;
-   char device[128];
+    struct udev* udev;
+    struct udev_enumerate* enumerate;
+    struct udev_list_entry* devices;
+    struct udev_list_entry* dev_list_entry;
+    char device[128];
 
-   udev = udev_new();
-   if(!udev) {
-       printf("Error\n");
-       // TODO: exit;
-   }
+    udev = udev_new();
+    if(!udev) {
+        printf("Error\n");
+        // TODO: exit;
+    }
 
-   enumerate = udev_enumerate_new(udev);
-   if(!enumerate) {
-       printf("Error\n");
-       // TODO: exit;
-   }
-
-
-
+    enumerate = udev_enumerate_new(udev);
+    if(!enumerate) {
+        printf("Error\n");
+        // TODO: exit;
+    }
 }
 
-int main() {
+int main()
+{
     printf("Hello, World!\n");
     return 0;
 }
