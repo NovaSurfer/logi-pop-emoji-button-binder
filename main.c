11eacf3fd4f2732054d0c1b5587f0fed991071dd
Max Ruts <tribar.ge@gmail.com>
2023-02-13 03:18:14 +0100

    Finds needed device from syspath

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <libudev.h>
 #include <stdio.h>
+#include <string.h>
 
 void hid_open(unsigned short vendor_id, unsigned short product_id)
 {
@@ -8,7 +9,7 @@ void hid_open(unsigned short vendor_id, unsigned short product_id)
     struct udev_enumerate* enumerate;
     struct udev_list_entry* devices;
     struct udev_list_entry* dev_list_entry;
-    char device[128];
+    //char device[128];
 
     udev = udev_new();
     if(!udev) {
@@ -40,12 +41,27 @@ void hid_open(unsigned short vendor_id, unsigned short product_id)
         const char* path;
 
         path = udev_list_entry_get_name(dev_list_entry);
-        dev = udev_device_new_from_syspath(udev, path);
 
-        printf("dev_path: %s\n", udev_device_get_syspath(dev));
+        // 'path' format is something like this:
+        // /sys/devices/virtual/misc/uhid/0005:046D:B030.000D/hidraw/hidraw1
+        // so let's find the 'uhind' dir
+        const char* hid_start = strstr(path, "uhid");
+        if(hid_start) {
+            // and skip 10 bytes (uhid/0005:) with dir name and device bus
+            // read the string staring from the device vendor ID.
+            int ret = sscanf(hid_start + 10, "%hx:%hx", &dev_vid, &dev_pid);
 
-        // free dev
-        udev_device_unref(dev);
+            if(ret == 2 && dev_vid == vendor_id && dev_pid == product_id) {
+                dev = udev_device_new_from_syspath(udev, path);
+                printf("dev_path: %s\n", udev_device_get_syspath(dev));
+                printf("Product ID: %hx Vendor ID: %hx\n", dev_vid, dev_pid);
+                // free dev
+                udev_device_unref(dev);
+            } else {
+                printf("Device not found\n");
+                // TODO: exit;
+            }
+        }
     }
 
     // clean up
