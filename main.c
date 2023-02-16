8dcec359fb802feafd7678893552df6071840697
Max Ruts <tribar.ge@gmail.com>
2023-02-16 02:42:49 +0100

    Opens & reads device file descriptor

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <libudev.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 void hid_open(unsigned short vendor_id, unsigned short product_id)
 {
@@ -38,25 +41,51 @@ void hid_open(unsigned short vendor_id, unsigned short product_id)
     {
         unsigned short dev_vid = 0;
         unsigned short dev_pid = 0;
-        const char* path;
+        const char* dev_path;
 
-        path = udev_list_entry_get_name(dev_list_entry);
+        dev_path = udev_list_entry_get_name(dev_list_entry);
 
         // 'path' format is something like this:
         // /sys/devices/virtual/misc/uhid/0005:046D:B030.000D/hidraw/hidraw1
-        // so let's find the 'uhind' dir
-        const char* hid_start = strstr(path, "uhid");
+        // so let's find the 'uhid' dir
+        const char* hid_start = strstr(dev_path, "uhid");
         if(hid_start) {
             // and skip 10 bytes (uhid/0005:) with dir name and device bus
             // read the string staring from the device vendor ID.
             int ret = sscanf(hid_start + 10, "%hx:%hx", &dev_vid, &dev_pid);
 
             if(ret == 2 && dev_vid == vendor_id && dev_pid == product_id) {
-                dev = udev_device_new_from_syspath(udev, path);
-                printf("dev_path: %s\n", udev_device_get_syspath(dev));
-                printf("Product ID: %hx Vendor ID: %hx\n", dev_vid, dev_pid);
+                dev = udev_device_new_from_syspath(udev, dev_path);
+                const char* dev_node_path = udev_device_get_devnode(dev);
                 // free dev
                 udev_device_unref(dev);
+
+                printf("dev_path: %s\n", dev_path);
+                printf("dev_node_path: %s\n", dev_node_path);
+                printf("Vendor ID: %hx Product ID: %hx\n", dev_vid, dev_pid);
+
+                int fd;
+                int res;
+                char buf[256];
+
+                fd = open(dev_node_path, O_RDWR | O_NONBLOCK);
+                if(fd < 0) {
+                    printf("Error while opening device descriptor: %s\n", strerror(errno));
+                    // TODO: exit;
+                }
+
+                memset(buf, 0x0, sizeof(buf));
+
+                res = read(fd, buf, 16);
+                if(res < 0) {
+                    printf("ERROR: %s\n", strerror(errno));
+                } else {
+                    for(int i = 0; i < res; ++i) {
+                        printf("%hhx \n", buf[i]);
+                    }
+                }
+                close(fd);
+
             } else {
                 printf("Device not found\n");
                 // TODO: exit;
