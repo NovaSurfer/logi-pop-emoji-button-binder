31775d91f279deca271413f6a17542d21d6b84df
Max Ruts <tribar.ge@gmail.com>
2023-02-28 01:31:11 +0100

    Adds endless read from device file descriptor

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <libudev.h>
+#include <poll.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -41,9 +42,8 @@ void hid_open(unsigned short vendor_id, unsigned short product_id)
     {
         unsigned short dev_vid = 0;
         unsigned short dev_pid = 0;
-        const char* dev_path;
 
-        dev_path = udev_list_entry_get_name(dev_list_entry);
+        const char* dev_path = udev_list_entry_get_name(dev_list_entry);
 
         // 'path' format is something like this:
         // /sys/devices/virtual/misc/uhid/0005:046D:B030.000D/hidraw/hidraw1
@@ -57,34 +57,40 @@ void hid_open(unsigned short vendor_id, unsigned short product_id)
             if(ret == 2 && dev_vid == vendor_id && dev_pid == product_id) {
                 dev = udev_device_new_from_syspath(udev, dev_path);
                 const char* dev_node_path = udev_device_get_devnode(dev);
-                // free dev
-                udev_device_unref(dev);
 
                 printf("dev_path: %s\n", dev_path);
                 printf("dev_node_path: %s\n", dev_node_path);
                 printf("Vendor ID: %hx Product ID: %hx\n", dev_vid, dev_pid);
 
-                int fd;
-                int res;
-                char buf[256];
-
-                fd = open(dev_node_path, O_RDWR | O_NONBLOCK);
+                // Open
+                int fd = open(dev_node_path, O_RDWR | O_CLOEXEC);
                 if(fd < 0) {
                     printf("Error while opening device descriptor: %s\n", strerror(errno));
                     // TODO: exit;
                 }
 
+                char buf[256];
                 memset(buf, 0x0, sizeof(buf));
 
-                res = read(fd, buf, 16);
-                if(res < 0) {
-                    printf("ERROR: %s\n", strerror(errno));
-                } else {
-                    for(int i = 0; i < res; ++i) {
-                        printf("%hhx \n", buf[i]);
+                // Read
+                while(1) {
+                    int res = read(fd, buf, 16);
+                    if(res < 0) {
+                        if(errno == EAGAIN || errno == EINPROGRESS) {
+                            res = 0;
+                        } else {
+                            printf("ERROR: %s\n", strerror(errno));
+                        }
+                    } else {
+                        for(int i = 0; i < res; ++i) {
+                            printf("%hhx", buf[i]);
+                        }
+                        printf("\n");
                     }
                 }
                 close(fd);
+                // free dev
+                udev_device_unref(dev);
 
             } else {
                 printf("Device not found\n");
@@ -101,6 +107,6 @@ void hid_open(unsigned short vendor_id, unsigned short product_id)
 int main()
 {
     printf("Hello, World!\n");
-    hid_open(0x46d, 0xb030);
+    hid_open(0x046d, 0xb030);
     return 0;
 }
